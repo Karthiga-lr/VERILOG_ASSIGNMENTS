40aeac5809ed825806f8efb6efeb96c558f1b7c2
Karthiga-lr <lr.karthiga20@gmail.com>
2025-06-10 15:06:33 +0530

    Create Nand_gate_tb.v

---
+++ ASSIGNMENT_1/NAND_GATE/Nand_gate_tb.v
@@ -0,0 +1,16 @@
+module nandgate_tb;
+  reg a,b;
+  wire y;
+  nandgate duv(.*);
+  initial begin
+    $dumpfile("test.vcd");
+    $dumpvars(1);
+    a=0;b=0;#5;
+    a=0;b=1;#5;
+    a=1;b=0;#5;
+    a=1;b=1;#5;
+  end
+  initial begin
+    $monitor("time=%0t a=%b b=%b y=%b,",$time,a,b,y);
+  end
+endmodule
